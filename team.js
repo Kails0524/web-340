9f3c2b33ec491bb63fd2847dde3c134d0e67a3a2
Kailee Stephens <kaistephens1@my365.bellevue.edu>
2022-06-12 21:30:28 -0500

    Delete team.js

--- team.js
+++
@@ -1,9 +0,0 @@
-class Team {
-    constructor(name, mascot, playerCount) {
-      this.name = name;
-      this.mascot = mascot;
-      this.playerCount = playerCount;
-    }
-  }
-  
-  module.exports = { Team };
