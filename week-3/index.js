cb7703cb239c2e8ec984e4a19ab20c54a5fb7838
Kailee Stephens <kaistephens1@my365.bellevue.edu>
2022-06-12 21:32:30 -0500

    Create index.js

---
+++ week-3/index.js
@@ -0,0 +1,9 @@
+
+var team = require("./team.js");
+var manager = require("./team-manager.js");
+
+console.log(teams)
+
+//Imports array of team names and getGame string.
+import * as findTeams from "./team-manager.js";
+import * as getGame from "./team-manager.js";
